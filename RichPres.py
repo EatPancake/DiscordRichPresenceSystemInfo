8cbf55f40f0dc6a506f3011058f356afa8b06b7c
EatPancake <104321961+EatPancake@users.noreply.github.com>
2022-12-14 16:56:02 -0500

    gpu support
    add support for nvida gpus

--- RichPres.py
+++ RichPres.py
@@ -2,6 +2,14 @@ from pypresence import Presence
 import time
 import platform
 import psutil
+import GPUtil
+
+def NvidaGpu():
+    GPUs = GPUtil.getGPUs()
+    if (len(GPUs) > 0):
+        return True
+    else:
+        return False
 
 client_id = "1050923361527664751"
 
@@ -11,6 +19,13 @@ winversion = str(platform.release())
 winVersion = winsystem + " " + winversion
 
 startTime = int(time.time())
+
+cpuType = platform.uname().machine
+
+ramSize = (psutil.virtual_memory().total) / (1000000000);
+
+Nvida = NvidaGpu()
+gpuLoad = ""
 
 RPC = Presence(client_id=client_id)
 RPC.connect()
@@ -23,6 +38,12 @@ while True:
     cpu = str(psutil.cpu_percent())
     ram = str(psutil.virtual_memory().percent)
 
+    if(Nvida == True):
+        GPUs = GPUtil.getGPUs()
+        load = GPUs[0].load
+        GPUname = GPUs[0].name
+        gpuLoad = str("GPU (" + GPUname +") "+ str(load) + "%")
+
     print(cpu)
-    RPC.update(state=("CPU " + cpu + "% RAM " + ram + "%"),start=int(startTime),details=winVersion)
+    RPC.update(state=("CPU (" + str(cpuType) + ") " + cpu + "% | RAM (" + str(round(ramSize)) + "GB) " + ram + "% | " + gpuLoad),start=int(startTime),details=winVersion)
     time.sleep(10)
